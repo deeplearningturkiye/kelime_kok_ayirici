43f2dad80a142757d54fc37fb245ed48d98cf516
birolkuyumcu <bluekid70@gmail.com>
2018-05-02 08:46:40 +0300

    removed old model files new files produced at models/ directory

--- train/train.py
+++ train/train.py
@@ -8,12 +8,12 @@ Date : Change
 ...
 """
 
-import codecs
 import numpy as np
 import os
 import time
 import json
 import pickle
+import pandas as pd
 
 from keras.models import Sequential,Model
 from keras.layers import Dense, Dropout, Activation, Flatten,Input
@@ -22,18 +22,19 @@ from keras.layers import TimeDistributed, Bidirectional,Reshape
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau,ModelCheckpoint,CSVLogger
 from sklearn.model_selection import train_test_split
 
-fp = codecs.open('KOKBULTEST.txt','r',encoding='utf-8')
-lines = fp.readlines()
-lines  = [l.strip() for l in lines]
+df = pd.read_csv('../data/kelime_kok.csv',encoding='utf-8')
+kelimeler = df.kelime.tolist()
+kokler = [ x.split(':')[0] for x in df.kok.tolist()]
+del df
 
-chars = list(set(''.join(lines)))
-chars.append(' ')
-chars.append('q')
-chars.sort()
-chars =''.join(chars).strip()
-chars = chars.replace(',','')
-chars = ' '+chars
 
+fp = open('../data/datafile.pkl','rb')
+data = pickle.load(fp)
+fp.close()
+
+chars = data['chars']
+charlen = data['charlen']
+maxlen = data['maxlen']
 charlen = len(chars)
 maxlen = 22
 
@@ -62,20 +63,12 @@ def decode(mat):
     for i in range(mat.shape[0]):
         word += chars[np.argmax(mat[i,:])]
     return word.strip()
-	
-kelimeler = []
-kokler = []
-for line in lines:
-    words = line.split(',')
-    kelimeler.append(words[0])
-    kok = (words[1].split(':'))[0]
-    kokler.append(kok)
-	
+		
 X = []
 Y = []
 for w,k in zip(kelimeler,kokler):
-    w = encode(w)
-    k = encode(k)
+    w = encode(w,maxlen)
+    k = encode(k,maxlen)
     X.append(w)
     Y.append(k)
 X = np.array(X)
@@ -84,6 +77,7 @@ Y = np.array(Y)
 # Split training and test set
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=2017)
 
+# model description
 harf_input = Input(shape=(maxlen,charlen))
 flat = Flatten()(harf_input)
 atn = Dense(maxlen*charlen)(flat)
@@ -100,30 +94,22 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-data = {}
-data['maxlen'] = maxlen
-data['charlen'] = charlen
-data['chars'] = chars
-fp = open('datafile.pkl','wb')
-pickle.dump(data,fp)
-fp.close()
-
 mjs = model.to_json()
-with open('kokbul.json', 'w') as outfile:
+with open('models/kokbul.json', 'w') as outfile:
     json.dump(mjs, outfile)
 	
 early = EarlyStopping(monitor='val_acc', min_delta=0, patience=9, verbose=1, mode='auto')
 redc = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=2, verbose=1, mode='auto', min_lr=0.00001)
 
-model_checkpoint = ModelCheckpoint(filepath="kokbul-{epoch:02d}-{val_acc:.2f}.hdf5", 
+model_checkpoint = ModelCheckpoint(filepath="models/kokbul-{epoch:02d}-{val_acc:.3f}.hdf5", 
                                    monitor='val_acc', save_best_only=True,mode='auto',save_weights_only=True,verbose=1)
 
-logfile = 'new_traing_'+('_'.join([str(x) for x in time.localtime()[:6]]))+'.log'
+logfile = 'models/new_traing_'+('_'.join([str(x) for x in time.localtime()[:6]]))+'.log'
 csv_logger = CSVLogger(logfile)
 cblist = [model_checkpoint,redc,csv_logger]
 
 model.fit(np.float32(x_train), np.float32(y_train),
-          batch_size=1,
+          batch_size=16,
           epochs=33,
           validation_data=(np.float32(x_test), np.float32(y_test)),callbacks=cblist,
           shuffle=True)
--- train/usage.py
+++ train/usage.py
@@ -6,6 +6,7 @@
 Module Name : usage kelime_kok_ayirici 
 Date : Change
 03/02/2017 : initial write
+02/05/2017 : new data file and trained models updated
 ...
 """
 
@@ -14,7 +15,7 @@ import numpy as np
 import datetime
 import random
 
-import codecs
+import pandas as pd
 import numpy as np
 import os
 import time
@@ -22,15 +23,15 @@ import json
 import pickle
 from keras.models import model_from_json
 
-jstr = json.loads(open('kokbul.json').read())
+jstr = json.loads(open('models/kokbul.json').read())
 model = model_from_json(jstr)
-model.load_weights('kokbul.hdf5')
+model.load_weights('models/kokbul-16-0.983.hdf5')
 
-fp = codecs.open('KOKBULTEST.txt','r',encoding='utf-8')
-lines = fp.readlines()
-lines  = [l.strip() for l in lines]
+df = pd.read_csv('../data/kelime_kok.csv',encoding='utf-8')
+kelimeler = df.kelime.tolist()
+kokler = [ x.split(':')[0] for x in df.kok.tolist()]
 
-fp = open('datafile.pkl','rb')
+fp = open('../data/datafile.pkl','rb')
 data = pickle.load(fp)
 fp.close()
 
@@ -64,14 +65,6 @@ def decode(mat):
         word += chars[np.argmax(mat[i,:])]
     return word.strip()
 	
-kelimeler = []
-kokler = []
-for line in lines:
-    words = line.split(',')
-    kelimeler.append(words[0])
-    kok = (words[1].split(':'))[0]
-    kokler.append(kok)
-
 words = []	
 X = []
 Y = []
@@ -81,12 +74,10 @@ for j in range(ntest):
 	w = kelimeler[i]
 	words.append(w)
 	k = kokler[i]
-	w = encode(w)
-	#k = encode(k)
+	w = encode(w,maxlen)
 	X.append(w)
 	Y.append(k)
 X = np.array(X)
-#Y = np.array(Y)
 
 yp = model.predict(X)
 
@@ -95,8 +86,14 @@ ktrue = 0.0
 for j in range(ntest):
 	print "\n\nWord      : ",words[j]
 	print "Kok       : ",Y[j]
-	print "Predicted : ",decode(yp[j])
-	if Y[j] == decode(yp[j]):
+	predicted_kok = decode(yp[j])
+	
+	# kural tabanlı kontrol ve düzeltme
+	if len(words[j]) < len(predicted_kok) :
+		predicted_kok = predicted_kok[:len(words[j])]
+		
+	print "Predicted : ", predicted_kok
+	if Y[j] == predicted_kok:
 		ktrue +=1.0
 		
 print "Accuracy %0.02f"%(100*ktrue/ntest)
